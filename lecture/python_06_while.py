829f51c319934dfcc1a2a1ca3afba2bd1da01f53
dltjwns1213 <133830062+dltjwns1213@users.noreply.github.com>
2023-10-06 12:37:10 +0900

    Add files via upload
    123

---
+++ lecture/python_06_while.py
@@ -0,0 +1,22 @@
+# while 반복문
+# - 반복횟수를 모르는 경우
+# - 조건이 만족하는 동안 계속 반복
+# - 조건이 True이면 무한 반복
+# - 조건이 false이면 반복 종료
+# - while문에 조건 True => 무한 loop문(조심!!)
+#  -> 반드시 break문과 함께 사용할것!
+
+# while 기본 문법
+# while 조건:
+#       실행문
+
+a = list(range(1,6)) # [1, 2, 3, 4, 5]
+print(a)
+i = 0 # index
+
+while i < len(a):
+    print(a[i])
+    i += 1
+
+
+
